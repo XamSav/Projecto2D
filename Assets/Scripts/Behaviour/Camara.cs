9dc0b1b27513bc3d1c060d9462869a4b1f9bf5b9
Kiznaiver27 <Kiznaiver27@github.com>
2020-02-18 20:57:28 +0100

    Update 1.0 - Samuel

---
+++ Assets/Scripts/Behaviour/Camara.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Behaviour/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+    public Slider volumen;
+    public Toggle subtitulos;
+    private void Awake()
+    {
+        volumen.value = PlayerPrefs.GetFloat("Volumen");
+        subtitulos.isOn = PlayerPrefs.GetInt("Subtitulos") == 1 ? true : false;
+    }
+    public void SaveOptions()
+    {
+        PlayerPrefs.SetFloat("Volumen", volumen.value);
+        int k = subtitulos.isOn ? 1 : 0;
+        PlayerPrefs.SetInt("Subtitulos", k);
+
+        PlayerPrefs.Save();
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1f;
+    }
+}
