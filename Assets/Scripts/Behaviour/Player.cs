204bcbe007346758d51e51feb0fb6d024297eff2
Kiznaiver27 <Kiznaiver27@github.com>
2020-02-20 20:44:50 +0100

    Animaciones 1.0
    Solo hay animacion de Abajo

---
+++ Assets/Scripts/Behaviour/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hits = 7;
+
+    private Move _move;
+    void Awake()
+    {
+        _move = GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            _move.move(new Vector2(-1, 1));
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+            {
+                _move.move(new Vector2(1, 1));
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+                {
+                    _move.move(new Vector2(-1, -1));
+                }
+                else {
+                    if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+                    {
+                        _move.move(new Vector2(1, -1));
+                    }
+                    else {
+                        if (Input.GetKey(KeyCode.W))
+                        {
+                            _move.move(new Vector2(0, 1));
+                        }
+                        if (Input.GetKey(KeyCode.S))
+                        {
+                            _move.move(new Vector2(0, -1));
+                        }
+                        if (Input.GetKey(KeyCode.D))
+                        {
+                            _move.move(new Vector2(1, 0));
+                        }
+                        if (Input.GetKey(KeyCode.A))
+                        {
+                            _move.move(new Vector2(-1, 0));
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
