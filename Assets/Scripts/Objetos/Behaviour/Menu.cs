48251324f5d216b6e2e99bead04973573cb18081
Jose <a19josveicam@inspedralbes.cat>
2020-03-03 20:54:12 +0100

    Update - 3.0.0 Jose

--- Assets/Scripts/Objetos/Behaviour/Menu.cs
+++ Assets/Scripts/Objetos/Behaviour/Menu.cs
@@ -5,18 +5,15 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public Slider volumen;
-    public Toggle subtitulos;
+
     private void Awake()
     {
         volumen.value = PlayerPrefs.GetFloat("Volumen");
-        subtitulos.isOn = PlayerPrefs.GetInt("Subtitulos") == 1 ? true : false;
+      
     }
     public void SaveOptions()
     {
         PlayerPrefs.SetFloat("Volumen", volumen.value);
-        int k = subtitulos.isOn ? 1 : 0;
-        PlayerPrefs.SetInt("Subtitulos", k);
-
         PlayerPrefs.Save();
     }
     public void Pause()
---
+++ Assets/Scripts/Objetos/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Move _move;
+    [SerializeField]
+    private bool _aggro;
+
+    float seconds;
+    int randomdir;
+
+    void Awake()
+    {
+        _move = GetComponent<Move>();
+        seconds = 3;
+        randomdir= Random.Range(0, 4);
+    }
+
+    void Update()
+    {
+        
+        seconds -= Time.deltaTime;
+
+
+        switch (randomdir)
+        {
+            case 0:
+                _move.move(new Vector2(0, 1));
+                break;
+            case 1:
+                _move.move(new Vector2(0, -1));
+                break;
+            case 2:
+                _move.move(new Vector2(1, 0));
+                break;
+            case 3:
+                _move.move(new Vector2(-1, 0));
+                break;
+        }
+    }
+}
