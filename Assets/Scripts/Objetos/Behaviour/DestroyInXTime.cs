91f90fcba1389212c70c3e4f5ef6aad3a248e367
Kiznaiver27 <Kiznaiver27@github.com>
2020-03-08 15:44:32 +0100

    Update - 4.0.0
    Control de PS4 con animaciones, Colliders Aldea.

---
+++ Assets/Scripts/Objetos/Behaviour/DestroyInXTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyInXTime : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 0.5f;
+    [SerializeField]
+    private GameObject objectDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Destroy");
+    }
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(time);
+        Debug.Log("Hey!");
+        Destroy(objectDestroy);
+    }
+}
--- Assets/Scripts/Objetos/Player.cs
+++ Assets/Scripts/Objetos/Player.cs
@@ -9,22 +9,69 @@ public class Player : MonoBehaviour
     private Animator _animator;
     [SerializeField]
     private GameObject _axe;
-
-
+    private float secondsCounter = 0;
+    private float secondsToCount = 1;
     void Awake()
     {        
         _move = GetComponent<Move>();
     }
     private void Start()
     {
-        _animator=GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
         DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Up-Left
+        Vector3 _movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Magnitude", _movement.magnitude);
+
+        _move.move(_movement.normalized);
+
+
+
+        /*
+        float moveUp = Input.GetAxis("Vertical")* _speed;
+        float moveRight = Input.GetAxis("Horizontal")*_speed;
+        moveUp *= Time.deltaTime;
+        moveRight *= Time.deltaTime;
+        if (moveUp != 0)
+        {
+            _move.move(transform.up*moveUp);
+            if(moveUp > 0)
+            {
+                _animator.SetInteger("State", 5);
+            }
+            else
+            {
+                _animator.SetInteger("State", 1);
+            }
+        }
+        else if( moveRight != 0)
+        {
+            _move.move(transform.right * moveRight);
+            if (moveRight > 0)
+            {
+                _animator.SetInteger("State", 3);
+            }
+            else
+            {
+                _animator.SetInteger("State", 7);
+            }
+        }*/
+
+
+
+
+
+
+
+
+        /*//Up-Left
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
             _animator.SetInteger("State",6);
@@ -74,20 +121,24 @@ public class Player : MonoBehaviour
                             _animator.SetInteger("State", 7);
                             _move.move(Vector2.left);
                         }
-                      /*  if (!Input.anyKey){
-                            _animator.SetInteger("State", 0);
-                        }*/
+                      //  if (!Input.anyKey){
+                      //      _animator.SetInteger("State", 0);
+                      //  }
                     }
-                }
+                } 
             }
-            if (Input.GetKeyDown(KeyCode.Space))
+        }*/
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (secondsCounter >= secondsToCount)
             {
+                secondsCounter = 0;
                 GameObject hijo = Instantiate(_axe) as GameObject;
                 hijo.transform.parent = this.transform;
                 hijo.transform.position = this.transform.position;
             }
-
+            
         }
-       
+        secondsCounter += Time.deltaTime;
     }
 }
