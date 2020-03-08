2d0630e1966a99f88f4aa8870c6dd92fa2b59c01
Kiznaiver27 <Kiznaiver27@github.com>
2020-03-08 19:27:09 +0100

    Update - 4.0.2

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 public class Menu : MonoBehaviour
 {
-    public Slider volumen;
+    
+    [SerializeField]
+    private Slider volumen;
+    private bool _controlleroption = true;
     private void Awake()
     {
         volumen.value = PlayerPrefs.GetFloat("Volumen");
@@ -25,5 +29,23 @@ public class Menu : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+    public void HandleInputData(int val)
+    {
+        //Mando
+        if (val == 1)
+        {
+            _controlleroption = true;
+            Debug.Log(_controlleroption);
+            
+
+        }
+        //PC
+        if (val == 0)
+        {
+            _controlleroption = false;
+            Debug.Log(_controlleroption);
+            
+        }
     }
 }
--- Assets/Scripts/Objetos/Player.cs
+++ Assets/Scripts/Objetos/Player.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //private int hits = 7;
-
+    public Menu menu;
     private Move _move;
     private Animator _animator;
     [SerializeField]
     private GameObject _axe;
+    private int controller;
     private float secondsCounter = 0;
     private float secondsToCount = 1;
     void Awake()
-    {        
+    {
         _move = GetComponent<Move>();
     }
     private void Start()
@@ -21,113 +22,31 @@ public class Player : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
+    public void HandleInputData(int val)
+    {
+            controller = val;
+    }
+
     void Update()
     {
-        Vector3 _movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-
-        _animator.SetFloat("Horizontal", _movement.x);
-        _animator.SetFloat("Vertical", _movement.y);
-        _animator.SetFloat("Magnitude", _movement.magnitude);
-
-        _move.move(_movement.normalized);
-
-
-
-        /*
-        float moveUp = Input.GetAxis("Vertical")* _speed;
-        float moveRight = Input.GetAxis("Horizontal")*_speed;
-        moveUp *= Time.deltaTime;
-        moveRight *= Time.deltaTime;
-        if (moveUp != 0)
+        if (controller == 0)
         {
-            _move.move(transform.up*moveUp);
-            if(moveUp > 0)
-            {
-                _animator.SetInteger("State", 5);
-            }
-            else
-            {
-                _animator.SetInteger("State", 1);
-            }
-        }
-        else if( moveRight != 0)
-        {
-            _move.move(transform.right * moveRight);
-            if (moveRight > 0)
-            {
-                _animator.SetInteger("State", 3);
-            }
-            else
-            {
-                _animator.SetInteger("State", 7);
-            }
-        }*/
-
-
-
-
-
-
-
-
-        /*//Up-Left
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
-        {
-            _animator.SetInteger("State",6);
-            _move.move(new Vector2(-1, 1).normalized);
+            Debug.Log("Hey!");
+            Vector3 _movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+            _animator.SetFloat("Horizontal", _movement.x);
+            _animator.SetFloat("Vertical", _movement.y);
+            _animator.SetFloat("Magnitude", _movement.magnitude);
+            _move.move(_movement.normalized);
         }
         else
         {
-            //Up-Right
-            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-            {
-                _animator.SetInteger("State",4);
-                _move.move(new Vector2(1, 1).normalized);
-            }
-            else
-            {
-                //Down-Left
-                if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-                {
-                    _animator.SetInteger("State",8);
-                    _move.move(new Vector2(-1, -1).normalized);
-                }
-                else {
-                    //Down-Right
-                    if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-                    {
-                        _animator.SetInteger("State",2);
-                        _move.move(new Vector2(1, -1).normalized);
-                    }
-                    else {
-                        if (Input.GetKey(KeyCode.W))
-                        {
-                            _animator.SetInteger("State",5);
-                            _move.move(Vector2.up);
-                        }
-                        if (Input.GetKey(KeyCode.S))
-                        {
-                            _animator.SetInteger("State",1);
-                            _move.move(Vector2.down);
-                        }
-                        if (Input.GetKey(KeyCode.D))
-                        {
-                            _animator.SetInteger("State",3);
-                            _move.move(Vector2.right);
-                        }
-                        if (Input.GetKey(KeyCode.A))
-                        {
-                            _animator.SetInteger("State", 7);
-                            _move.move(Vector2.left);
-                        }
-                      //  if (!Input.anyKey){
-                      //      _animator.SetInteger("State", 0);
-                      //  }
-                    }
-                } 
-            }
-        }*/
+            Debug.Log("Hey!");
+            Vector3 _movement = new Vector3(Input.GetAxis("HorizontalS"), Input.GetAxis("VerticalS"), 0.0f);
+            _animator.SetFloat("Horizontal", _movement.x);
+            _animator.SetFloat("Vertical", _movement.y);
+            _animator.SetFloat("Magnitude", _movement.magnitude);
+            _move.move(_movement.normalized);
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (secondsCounter >= secondsToCount)
@@ -137,7 +56,6 @@ public class Player : MonoBehaviour
                 hijo.transform.parent = this.transform;
                 hijo.transform.position = this.transform.position;
             }
-            
         }
         secondsCounter += Time.deltaTime;
     }
