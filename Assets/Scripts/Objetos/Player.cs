4f053d030154537c91c9647a43d8846cd7274713
Jose <a19josveicam@inspedralbes.cat>
2020-03-10 20:19:40 +0100

    Bridge Update

--- Assets/Scripts/Objetos/Player.cs
+++ Assets/Scripts/Objetos/Player.cs
@@ -41,11 +41,95 @@ public class Player : MonoBehaviour
         else
         {
             Debug.Log("Hey!");
-            Vector3 _movement = new Vector3(Input.GetAxis("HorizontalS"), Input.GetAxis("VerticalS"), 0.0f);
+            /*Vector3 _movement = new Vector3(Input.GetAxis("HorizontalS"), Input.GetAxis("VerticalS"), 0.0f);
             _animator.SetFloat("Horizontal", _movement.x);
             _animator.SetFloat("Vertical", _movement.y);
             _animator.SetFloat("Magnitude", _movement.magnitude);
-            _move.move(_movement.normalized);
+            _move.move(_movement.normalized);*/
+            //Up-Left
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+            {
+                Vector2 _movement = new Vector2(-1, 1);
+                _animator.SetFloat("Horizontal", _movement.x);
+                _animator.SetFloat("Vertical", _movement.y);
+                _animator.SetFloat("Magnitude", _movement.magnitude);
+                _move.move(_movement.normalized);
+            }
+            else
+            {
+                //Up-Right
+                if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+                {
+                    Vector2 _movement = new Vector2(1, 1);
+                    _animator.SetFloat("Horizontal", _movement.x);
+                    _animator.SetFloat("Vertical", _movement.y);
+                    _animator.SetFloat("Magnitude", _movement.magnitude);
+                    _move.move(_movement.normalized);
+                }
+                else
+                {
+                    //Down-Left
+                    if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+                    {
+                        Vector2 _movement = new Vector2(-1, -1);
+                        _animator.SetFloat("Horizontal", _movement.x);
+                        _animator.SetFloat("Vertical", _movement.y);
+                        _animator.SetFloat("Magnitude", _movement.magnitude);
+                        _move.move(_movement.normalized);
+                    }
+                    else
+                    {
+                        //Down-Right
+                        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+                        {
+                            Vector2 _movement = new Vector2(1, -1);
+                            _animator.SetFloat("Horizontal", _movement.x);
+                            _animator.SetFloat("Vertical", _movement.y);
+                            _animator.SetFloat("Magnitude", _movement.magnitude);
+                            _move.move(_movement.normalized);
+                        }
+                        else
+                        {
+                            if (Input.GetKey(KeyCode.W))
+                            {
+                                Vector2 _movement = new Vector2(0, 1);                                
+                                _animator.SetFloat("Vertical", _movement.y);
+                                _animator.SetFloat("Horizontal", _movement.x);
+                                _animator.SetFloat("Magnitude", _movement.magnitude);
+                                _move.move(Vector2.up);
+                            }
+                            if (Input.GetKey(KeyCode.S))
+                            {
+                                Vector2 _movement = new Vector2(0, -1);
+                                _animator.SetFloat("Vertical", _movement.y);
+                                _animator.SetFloat("Horizontal", _movement.x);
+                                _animator.SetFloat("Magnitude", _movement.magnitude);
+                                _move.move(Vector2.down);
+                            }
+                            if (Input.GetKey(KeyCode.D))
+                            {
+                                Vector2 _movement = new Vector2(1, 0);
+                                _animator.SetFloat("Vertical", _movement.y);
+                                _animator.SetFloat("Horizontal", _movement.x);
+                                _animator.SetFloat("Magnitude", _movement.magnitude);
+                                _move.move(Vector2.right);
+                            }
+                            if (Input.GetKey(KeyCode.A))
+                            {
+                                Vector2 _movement = new Vector2(-1, 0);
+                                _animator.SetFloat("Vertical", _movement.y);
+                                _animator.SetFloat("Horizontal", _movement.x);
+                                _animator.SetFloat("Magnitude", _movement.magnitude);
+                                _move.move(Vector2.left);
+                            }
+                            /*  if (!Input.anyKey){
+                                  _animator.SetInteger("State", 0);
+                              }*/
+                        }
+                    }
+                }
+
+            }
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
